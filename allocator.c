5a1b03c380e69b91795b5a2f225530b8ebc5999b
Jonathan03ant <113400649+Jonathan03ant@users.noreply.github.com>
2023-12-24 15:11:25 -0500

    Malloc and Free Impelemented

--- allocator.c
+++ allocator.c
@@ -2,9 +2,13 @@
 #include "allocator.h"
 #include <sys/mman.h>
 
+/*
+    *Implementing malloc
+        *size is passed, this is the amount of memory block we need to allocate
+*/
 void* jalloc (size_t size){
 
-    memory_block* header;
+    memory_blockHeader* header;
 
     /* If size is zero, return null */
 
@@ -34,7 +38,7 @@ void* jalloc (size_t size){
     size_t total_size;
     void* block;
 
-    total_size = sizeof(memory_block) + size;
+    total_size = sizeof(memory_blockHeader) + size;
     block = mmap(0, total_size, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
     if (block == MAP_FAILED){
 
@@ -60,4 +64,73 @@ void* jalloc (size_t size){
     /* *UNLOCK */
     pthread_mutex_unlock(&global_malloc_lock);
     return (void*)(header + 1);
+}
+
+
+/*
+    *Implementing free
+        *Block is passed to jfree, it is a pointer to the start of memory to be freed
+*/
+void jfree(void* block) {
+    memory_blockHeader* header;
+    memory_blockHeader* curr;
+
+    /*
+        *Check if the bloack to be freed is NULL
+    */
+
+    if (!block){
+        return NULL;
+    }
+
+    pthread_mutex_lock(&global_malloc_lock);
+    /*
+        *Retriving the header to access information about the memory bloack (UNION HEADER)
+    */
+    header = (memory_blockHeader*)block - 1;
+
+    /*
+        *After retriving the header (MEMORYBLOCK METADATA), we need to check the following
+            *If it is the end of our Linked List (header == tail) ---> or points to tail
+                --> we can release it to the OS
+            *Otherwise, i.e the block is in the middle, we can only mark it Free
+                --> Mark it free so we can use it later
+    */
+    if (header == tail){
+        /* 
+            Block To be freed is the only Element 
+            *Head-->Block-->Tail
+        */
+        if (header == tail){
+            head = tail = NULL;
+        }
+        /*
+            *Is there are other blocks, traverse the LL and find the tail, 
+                --> set the tail to NULL
+        */
+        else{
+            curr = head;
+            while(curr){
+                if(curr->mem.next == tail){
+                    curr->mem.next = NULL;
+                    tail = curr;
+                }
+                curr = curr->mem.next;
+            }
+        }
+
+        /*
+            *Finally, system call to actually release the memory
+        */
+        munmap(header, header->mem.size + sizeof(memory_blockHeader));
+        pthread_mutex_unlock(&global_malloc_lock);
+        return;
+    }
+    /*
+        *If the Block (From header) is not at the end, it is better if we mark it "FREE"
+            *So it cn=an be used later
+    */
+
+    header->mem.is_free = 1;
+    pthread_mutex_unlock(&global_malloc_lock);
 }
